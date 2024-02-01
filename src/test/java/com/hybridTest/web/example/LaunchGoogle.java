a06e2dfc610f518312edf8ed3b077db426aae20f
yeungemy <emyzheng80@gmail.com>
2024-01-31 20:04:13 -0500

    added files

---
+++ src/test/java/com/hybridTest/web/example/LaunchGoogle.java
@@ -0,0 +1,23 @@
+package com.hybridTest.web.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchGoogle {
+    @Test
+    public void launchGoogle() {
+        // Use WebDriverManager to set up ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        // Create an instance of the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Open a website
+        driver.get("https://www.google.com");
+
+        // Close the browser
+        driver.quit();
+    }
+}
