ade0a600b0686d63821cce76fb4fa296010523e7
Emy Yeung <emyzheng80@gmail.com>
2024-02-01 22:26:15 -0500

    updated print code statement lines

--- src/test/java/com/hybridTest/webTest/example/StringTest.java
+++ src/test/java/com/hybridTest/webTest/example/StringTest.java
@@ -9,12 +9,19 @@ public class StringTest {
 
     public void reverseStringTest(){
         char[] s = {'h', '1', '2', '3'};
-//        System.out.println(reverseString(s));
+        System.out.print("The original string: ");
+        System.out.println(s);
+        reverseString(s);
+        System.out.print("The reversed string: ");
+        System.out.println(s);
     }
 
     public void reverseString(char[] s) {
+        char temp;
         for(int i = 0, j = s.length - 1; i < s.length/2 ; i++, j--){
-
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
         }
     }
 }
