398b55cf5235e9ff40c45a7caf4ab54448dc938d
Emy Yeung <emyzheng80@gmail.com>
2024-02-01 22:17:01 -0500

    added two general tests

---
+++ src/test/java/com/hybridTest/webTest/example/StringTest.java
@@ -0,0 +1,20 @@
+package com.hybridTest.webTest.example;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class StringTest {
+    @Test
+
+    public void reverseStringTest(){
+        char[] s = {'h', '1', '2', '3'};
+//        System.out.println(reverseString(s));
+    }
+
+    public void reverseString(char[] s) {
+        for(int i = 0, j = s.length - 1; i < s.length/2 ; i++, j--){
+
+        }
+    }
+}
