6119f11793653546667a43cab5dc0d4d159cacfb
yeungemy <emyzheng80@gmail.com>
2024-02-01 22:13:57 -0500

    added hooks

--- src/test/java/com/hybridTest/webTest/example/ToolSearchFieldTest.java
+++ src/test/java/com/hybridTest/webTest/example/ToolSearchFieldTest.java
@@ -5,21 +5,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class ToolSearchFieldTest {
-    @Test
-    public void launchGoogle() {
-        // Use WebDriverManager to set up ChromeDriver
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // Set up chrome driver
         WebDriverManager.chromedriver().setup();
 
-        // Create an instance of the ChromeDriver
-        WebDriver driver = new ChromeDriver();
+        // Create an instance of chrome driver
+        driver = new ChromeDriver();
 
-        // Open a website
+        // Open the website
         driver.get("https://practicesoftwaretesting.com/#/");
 
+        // Maximize the window
+        driver.manage().window().maximize();
+
+        // Dynamically wait for the body to be present
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.tagName("body")));
+    }
+    @Test
+    public void launchGoogle() {
         String searchString = "Combination Pliers";
         WebElement inputField = driver.findElement(By.cssSelector("[data-test='search-query']"));
         inputField.sendKeys(searchString);
@@ -27,8 +43,13 @@ public class ToolSearchFieldTest {
         String inputFieldValue = inputField.getAttribute("value");
 
         Assert.assertEquals(inputFieldValue, searchString);
+    }
 
-        // Close the browser
-        driver.quit();
+    @AfterMethod
+    public void tearDown() {
+        // Quit the driver
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
--- src/test/java/com/hybridTest/webTest/example/ToolSortTest.java
+++ src/test/java/com/hybridTest/webTest/example/ToolSortTest.java
@@ -8,47 +8,65 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class ToolSortTest {
 
-    @Test
-    public void sortToolByNameInDescOrder() {
-        // set up chrome driver
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        // Set up chrome driver
         WebDriverManager.chromedriver().setup();
 
-        // create an instance of chrome driver
-        WebDriver driver = new ChromeDriver();
+        // Create an instance of chrome driver
+        driver = new ChromeDriver();
 
-        // open the website
+        // Open the website
         driver.get("https://practicesoftwaretesting.com/#/");
 
-        // get element of sort field
+        // Maximize the window
+        driver.manage().window().maximize();
+
+        // Dynamically wait for the body to be present
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(By.tagName("body")));
+    }
+
+    @Test
+    public void sortToolByNameInDescOrder() {
+        // Get element of sort field
         WebElement sortInputField = driver.findElement(By.cssSelector("[data-test=\"sort\"]"));
 
-        // click the field to open the dropdown list
+        // Click the field to open the dropdown list
         sortInputField.click();
 
-        // get the option of DESC name
+        // Get the option of DESC name
         WebElement descNameOption = driver.findElement(By.cssSelector("[value=\"name,desc\"]"));
 
         // Wait for the option to be clickable
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(descNameOption));
 
-        // select the option
+        // Select the option
         descNameOption.click();
 
-        // wait for the option to be selected
+        // Wait for the option to be selected
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.attributeContains(
                 sortInputField, "value", "name,desc"));
 
-        // verify the option has been selected
+        // Verify the option has been selected
         String optionName = sortInputField.getAttribute("value");
         Assert.assertEquals(optionName, "name,desc");
+    }
 
-        // quit the driver
-        driver.quit();
+    @AfterMethod
+    public void tearDown() {
+        // Quit the driver
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
