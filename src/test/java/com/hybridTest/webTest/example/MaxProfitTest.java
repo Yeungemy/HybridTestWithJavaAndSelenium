1f991f37a3a65b00649454efd5a07bad0ec81127
Emy Yeung <emyzheng80@gmail.com>
2024-02-01 23:25:29 -0500

    added best time to buy and sell:

---
+++ src/test/java/com/hybridTest/webTest/example/MaxProfitTest.java
@@ -0,0 +1,34 @@
+package com.hybridTest.webTest.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MaxProfitTest {
+
+    @Test
+    public void testMaxProfit() {
+        int[] prices1 = {7, 1, 5, 3, 6, 4};
+        System.out.println("Max profit for prices1: " + maxProfit(prices1)); // Output: 5
+        Assert.assertEquals(maxProfit(prices1), 5);
+
+        int[] prices2 = {7, 6, 4, 3, 1};
+        System.out.println("Max profit for prices2: " + maxProfit(prices2)); // Output: 0
+        Assert.assertEquals(maxProfit(prices2), 0);
+    }
+
+    private int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int profit = 0;
+        int minPrice = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(prices[i], minPrice);
+            profit = Math.max(profit, prices[i] - minPrice);
+        }
+
+        return profit;
+    }
+}
